99beedcc7f734af0b1c1faed5807f7101af8e9e8
Daniela Magrì <82176325+Danix002@users.noreply.github.com>
2025-07-22 19:13:54 +0200

    fix:  make the location permission request more robust

--- app/src/main/java/it/unibo/collektive/MainActivity.kt
+++ app/src/main/java/it/unibo/collektive/MainActivity.kt
@@ -4,6 +4,7 @@ package it.unibo.collektive
 
 import android.Manifest
 import android.content.Context
+import android.content.Intent
 import android.content.IntentSender
 import android.content.pm.PackageManager
 import android.os.Bundle
@@ -15,6 +16,7 @@ import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.IntentSenderRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresPermission
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -22,9 +24,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.twotone.LocationOn
-import androidx.compose.material.icons.twotone.Warning
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
@@ -34,6 +36,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.style.TextAlign
 import androidx.core.app.ActivityCompat
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -56,6 +60,8 @@ import it.unibo.collektive.ui.theme.Purple40
 import it.unibo.collektive.viewmodels.NearbyDevicesViewModel
 import it.unibo.collektive.viewmodels.CommunicationSettingViewModel
 import it.unibo.collektive.viewmodels.MessagesViewModel
+import android.provider.Settings
+import androidx.compose.runtime.remember
 
 /**
  * Main entry point for the Android app.
@@ -65,6 +71,14 @@ class MainActivity : ComponentActivity() {
     private lateinit var locationSettingsLauncher: ActivityResultLauncher<IntentSenderRequest>
     private lateinit var locationPermissionRequest: ActivityResultLauncher<Array<String>>
     private lateinit var locationCallback: LocationCallback
+
+    /**
+     * TODO: doc
+     */
+    override fun onResume() {
+        super.onResume()
+        permissionManager(start = false)
+    }
 
     /**
      * TODO: doc
@@ -80,7 +94,7 @@ class MainActivity : ComponentActivity() {
             if (result.resultCode == RESULT_OK) {
                 startApp(onRequestPermissions = { permissionManager(start = false) })
             } else {
-                accessDenied(this)
+                requestPermissionOutOfApp()
             }
         }
         locationPermissionRequest = registerForActivityResult(
@@ -94,7 +108,7 @@ class MainActivity : ComponentActivity() {
                     permissionManager(start = true)
                 }
                 else -> {
-                    accessDenied(this)
+                    requestPermissionOutOfApp()
                 }
             }
         }
@@ -167,10 +181,10 @@ class MainActivity : ComponentActivity() {
                     val intentSenderRequest = IntentSenderRequest.Builder(exception.resolution).build()
                     locationSettingsLauncher.launch(intentSenderRequest)
                 } catch (sendEx: IntentSender.SendIntentException) {
-                    accessDenied(this)
+                    requestPermissionOutOfApp()
                 }
             } else {
-                accessDenied(this)
+                requestPermissionOutOfApp()
             }
         }
     }
@@ -200,115 +214,79 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-    private fun requestPermissionOutOfApp(){
-        setContent {
-            CollektiveExampleAndroidTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Box(
-                        modifier = Modifier.padding(innerPadding).fillMaxSize(),
-                        contentAlignment = Alignment.Center
-                    ){
-                        Column(
-                            modifier = Modifier.width(300.dp),
-                            horizontalAlignment = Alignment.Start,
-                            verticalArrangement = Arrangement.spacedBy(20.dp)
-                        ) {
-                            Text(
-                                text = "1. Please go to settings",
-                                textAlign = TextAlign.Start
-                            )
-                            Text(
-                                text = "2. Give location access permission",
-                                textAlign = TextAlign.Start
-                            )
-                            Text(
-                                text = "3. Close and reopen the app",
-                                textAlign = TextAlign.Start
-                            )
-                            Box(
-                                modifier = Modifier.fillMaxWidth(),
-                                contentAlignment = Alignment.Center
-                            ) {
-                                Icon(
-                                    imageVector = Icons.TwoTone.LocationOn,
-                                    contentDescription = "Access denied",
-                                    tint = Purple40
-                                )
-                            }
-                        }
-                    }
+    @Composable
+    fun OpenAppSettings() {
+        val context = LocalContext.current
+        val activity = remember(context) { context as? ComponentActivity }
+
+        val annotatedString = buildAnnotatedString {
+            append("Please click here and give location access permission")
+        }
+
+        Button(
+            onClick = {
+                val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
+                    data = android.net.Uri.fromParts("package", context.packageName, null)
+                    flags = Intent.FLAG_ACTIVITY_NEW_TASK
                 }
-            }
+                context.startActivity(intent)
+                activity?.finish()
+            },
+            colors = ButtonDefaults.buttonColors(containerColor = Purple40, contentColor = Color.White)
+        ) {
+            Text(
+                text = annotatedString,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 12.dp, horizontal = 24.dp),
+                textAlign = TextAlign.Center,
+            )
         }
     }
 
     /**
      * TODO: doc
      */
-    private fun accessDenied(context: Context){
-        if (
-            ActivityCompat.checkSelfPermission(
-                context,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) == PackageManager.PERMISSION_GRANTED ||
-            ActivityCompat.checkSelfPermission(
-                context,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) == PackageManager.PERMISSION_GRANTED
-        ) {
-            setContent {
-                CollektiveExampleAndroidTheme {
-                    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+    private fun requestPermissionOutOfApp() {
+        setContent {
+            CollektiveExampleAndroidTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Box(
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ) {
                         Box(
-                            modifier = Modifier.padding(innerPadding).fillMaxSize(),
-                            contentAlignment = Alignment.Center
+                            modifier = Modifier
+                                .width(300.dp)
+                                .border(
+                                    width = 2.dp,
+                                    color = Purple40,
+                                    shape = RoundedCornerShape(8.dp)
+                                )
+                                .padding(16.dp)
                         ) {
                             Column(
-                                modifier = Modifier.fillMaxWidth(),
-                                horizontalAlignment = Alignment.CenterHorizontally
+                                horizontalAlignment = Alignment.Start,
+                                verticalArrangement = Arrangement.spacedBy(20.dp)
                             ) {
-                                Text(
-                                    text = "This app needs access to your location to work",
-                                    textAlign = TextAlign.Center
-                                )
-                                Icon(
-                                    imageVector = Icons.TwoTone.Warning,
-                                    contentDescription = "Access denied",
-                                    tint = Purple40
-                                )
-                                Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.Center) {
-                                    Button(
-                                        onClick = {
-                                            permissionManager(start = false)
-                                        },
-                                        colors = ButtonDefaults.buttonColors(
-                                            containerColor = Purple40,
-                                            contentColor = Color.White
-                                        )
-                                    ) {
-                                        Text(text = "Request Permission")
-                                    }
-                                }
-                                Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.Center) {
-                                    Button(
-                                        onClick = {
-                                            startApp(onRequestPermissions = { permissionManager(start = false) })
-                                        },
-                                        colors = ButtonDefaults.buttonColors(
-                                            containerColor = Purple40,
-                                            contentColor = Color.White
-                                        )
-                                    ) {
-                                        Text(text = "Continue")
-                                    }
+                                OpenAppSettings()
+                                Box(
+                                    modifier = Modifier.fillMaxWidth(),
+                                    contentAlignment = Alignment.Center
+                                ) {
+                                    Icon(
+                                        imageVector = Icons.TwoTone.LocationOn,
+                                        contentDescription = "Access denied",
+                                        tint = Purple40
+                                    )
                                 }
                             }
                         }
                     }
                 }
             }
-        }else{
-            requestPermissionOutOfApp()
         }
     }
 }
